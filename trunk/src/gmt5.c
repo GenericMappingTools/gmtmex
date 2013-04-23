2dfceb485e1c9df1e9b6076430ce32c3f1735dce
Joaquim Luis <jmfluis@gmail.com>
2013-04-23 21:23:22 +0000

    Now compiles a works (well the little bit tested) on Windows
    git-svn-id: svn://gmtserver.soest.hawaii.edu/gmt-mex@125 03974b7a-4592-4726-bb37-2cc38c695039

--- trunk/src/gmt5.c
+++ trunk/src/gmt5.c
@@ -27,48 +27,48 @@
  */
 
 #include "gmt_mex.h"
+#include "gmtmex_id.h"
+#include "gmtmex_keys.h"
+#include "gmtmex_progs.h"
 
 void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	int status = 0;				/* Status code from GMT API */
+	int module_id;    /* Module ID */
 	size_t str_length, k;
 	struct GMTAPI_CTRL *API = NULL;		/* GMT API control structure */
 	struct GMT_OPTION *options = NULL;	/* Linked list of options */
 	char *cmd = NULL;
-	char *keys = KEY;
 	char module[BUFSIZ];
-	enum Gmt_module_id;
+	int k_mod_nongmt = 120;
 
 	/* 1. Initializing new GMT session */
 	if ((API = GMT_Create_Session ("GMT5", 2U, 1U, GMTMEX_print_func)) == NULL) mexErrMsgTxt ("Failure to create GMT Session\n");
 
 	/* 2. Get mex arguments, if any, and extract the module name */
 	cmd = mxArrayToString (prhs[0]);		/* First argument is the command string, e.g., 'blockmean $ -R0/5/0/5 -I1' */
-	str_len = strlen  (cmd);			/* Length of command argument */
-	for (k = 0; k < str_length && cmd[k] != ' ') k++;	/* Determine first space in command */
+	str_length = strlen  (cmd);			/* Length of command argument */
+	for (k = 0; k < str_length && cmd[k] != ' '; k++);	/* Determine first space in command */
 	memset ((void *)module, 0, BUFSIZ*sizeof (char));
 	strncpy (module, cmd, k);			/* Isolate the module name */
 
 	/* 3. Determine the GMT module ID, or list module usages and return if not found */
 	if ((module_id = GMT_Get_Module (API, module)) == k_mod_nongmt) {
-		GMT_List_Module (API, NULL);
-		Return (EXIT_SUCCESS);
+		GMT_List_Module (API, -1);
+		return;
 	}
 
 	/* 4. Convert mex command line arguments to a linked option list */
-	if (GMT_Create_Options (API, 0, &cmd[k+1], &options)) mexErrMsgTxt ("Failure to parse GMT command options\n");
-	free (cmd);
+	if ((options = GMT_Create_Options (API, 0, &cmd[k+1])) == NULL) mexErrMsgTxt ("Failure to parse GMT command options\n");
 
 	/* 5. Parse the mex command, update GMT option lists, register in/out resources */
-	if (GMTMEX_parser (API, plhs, nlhs, prhs, nrhs, keys, options)) mexErrMsgTxt ("Failure to parse mex command options\n");
+	if (GMTMEX_parser (API, plhs, nlhs, prhs, nrhs, keys[module_id], options)) mexErrMsgTxt ("Failure to parse mex command options\n");
 	
 	/* 6. Run GMT cmd module, or give usage message if errors arise during parsing */
-	status = GMT_Call_Module (API, module_id], -1, options);
+	status = GMT_Call_Module (API, module_id, -1, options);
 
 	/* 7. Destroy linked option list */
 	if (GMT_Destroy_Options (API, &options)) mexErrMsgTxt ("Failure to destroy GMT options\n");
 
 	/* 8. Destroy GMT session */
 	if (GMT_Destroy_Session (API)) mexErrMsgTxt ("Failure to destroy GMT session\n");
-
-	exit (status);		/* Return the status from mex FUNC */
 }
