0f06be694a23e700f8e23a47faf47e8665112db2
Joaquim Luis <jmfluis@gmail.com>
2013-05-12 22:54:57 +0000

    Add ID keyword
    git-svn-id: svn://gmtserver.soest.hawaii.edu/gmt-mex/trunk@168 03974b7a-4592-4726-bb37-2cc38c695039

--- src/gmt.c
+++ src/gmt.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------
- *	$Id: gmt5.c 158 2013-05-05 20:51:12Z jluis $
+ *	$Id$
  *
  *	Copyright (c) 1991-$year by P. Wessel, W. H. F. Smith, R. Scharroo, J. Luis and F. Wobbe
  *	See LICENSE.TXT file for copying and redistribution conditions.
