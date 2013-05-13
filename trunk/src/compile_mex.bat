78d31f0c3529d4e6fbc9d5e7b076163f60e9a4dd
Joaquim Luis <jmfluis@gmail.com>
2013-05-13 13:42:22 +0000

    Update for gmt5 to gmt renaming
    git-svn-id: svn://gmtserver.soest.hawaii.edu/gmt-mex@174 03974b7a-4592-4726-bb37-2cc38c695039

--- trunk/src/compile_mex.bat
+++ trunk/src/compile_mex.bat
@@ -128,13 +128,13 @@ REM ----------------------------------------------------------------------------
 
 
 REM -------------------------------------------------------------------------------------------------------
-%CC% /c -DWIN32 %COMPFLAGS% -I%MATINC% -I%NETCDF_INC% -I%GMT_INC% %OPTIMFLAGS% %_MX_COMPAT% -DLIBRARY_EXPORTS -DGMT_MATLAB gmtmex_parser.c gmt5.c
+%CC% /c -DWIN32 %COMPFLAGS% -I%MATINC% -I%NETCDF_INC% -I%GMT_INC% %OPTIMFLAGS% %_MX_COMPAT% -DLIBRARY_EXPORTS -DGMT_MATLAB gmtmex_parser.c gmt5.c gmt.c
 
 
 rem link  /out:"mexparser.%MEX_EXT%" %LINKFLAGS% %NETCDF_LIB% %GMT_LIB% /implib:templib.x gmtmex_parser.obj gmt5.obj
 rem %CC% -DWIN32 %COMPFLAGS% -I%MATINC% -I%NETCDF_INC% -I%GMT_INC% %OPTIMFLAGS% %_MX_COMPAT% -DLIBRARY_EXPORTS -DGMT_MATLAB /DFUNC=GMT_grdproject /Fegrdproject mexprogram.c
 
-link  /out:"gmt5.%MEX_EXT%" %LINKFLAGS% %NETCDF_LIB% %GMT_LIB% /implib:templib.x gmtmex_parser.obj gmt5.obj
+link  /out:"gmt.%MEX_EXT%" %LINKFLAGS% %NETCDF_LIB% %GMT_LIB% /implib:templib.x gmtmex_parser.obj gmt.obj
 REM -------------------------------------------------------------------------------------------------------
 
 
