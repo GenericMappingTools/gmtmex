711a85584423af96b50bbfad56715101517bde56
Paul Wessel <pwessel@hawaii.edu>
2015-02-24 23:47:45 +0000

    Clean gmt.c ending
    git-svn-id: svn://gmtserver.soest.hawaii.edu/gmt-mex/trunk@293 03974b7a-4592-4726-bb37-2cc38c695039

--- src/gmt.c
+++ src/gmt.c
@@ -241,8 +241,10 @@ void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 					break;
 			}
 		}	/* else means we have an object used to pass arrays from Matlab into GMT */
-		else if (X[k].family == GMT_IS_TEXTSET)	/* Because Windows cannot stomack another DLL freeing strings */
-			GMTMEX_Free_Textset (API, X[k].object);
+		else {	/* Free any memory allocated outside of GMT */
+			if (X[k].family == GMT_IS_TEXTSET)	/* Because Windows cannot stomach another DLL freeing strings */
+				GMTMEX_Free_Textset (API, X[k].object);
+		}
 			
 		if (GMT_Destroy_Data (API, &X[k].object) != GMT_NOERROR)
 			mexErrMsgTxt ("GMT: Failed to destroy object used in the interface bewteen GMT and Matlab\n");
