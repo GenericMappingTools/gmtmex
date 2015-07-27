0fd0b2e03a4b8016f975f218aaf97c119979d5c0
Paul Wessel <pwessel@hawaii.edu>
2015-07-27 01:53:03 +0000

    Osx scripting blues
    git-svn-id: svn://gmtserver.soest.hawaii.edu/gmt-mex@330 03974b7a-4592-4726-bb37-2cc38c695039

--- trunk/prep_osxbundle.sh
+++ trunk/prep_osxbundle.sh
@@ -5,12 +5,13 @@
 # so gmtmex can link with the libXgmt library instead.
 # Run from /Applications/GMT-x.x.x/Contents/Resources/lib
 #-----------------------------------------------------------------
-mkdir -p mex
+mkdir -p mex/gmt/plugins
 ls *.dylib | egrep -v 'libgmt.dylib|libpsl.dylib' > /tmp/l.lis
 while read lib; do
 	new=`echo $lib | awk '{printf "libX%s\n", substr($1,4)}'`
 	cp $lib mex/$new
 done < /tmp/l.lis
+cp gmt/plugins/supplements.so mex/gmt/plugins/supplements.so
 cd mex
 ls *.dylib > /tmp/l.lis
 while read lib; do
@@ -32,5 +33,17 @@ while read lib; do
 done < /tmp/l.lis
 ln -s libXgmt.5.dylib libXgmt.dylib
 ln -s libXpsl.5.dylib libXpsl.dylib
+
+# Do plugin supplement
+echo "--> mex/gmt/plugins/supplements.so"
+echo
+cd gmt/plugins
+otool -L supplements.so | grep executable_path | awk '{print $1}' > /tmp/t.lis
+let k=1
+while read old; do
+	new=`echo $old | awk -F/ '{printf "libX%s\n", substr($NF,4)}'`
+	install_name_tool -change $old @rpath/$new supplements.so
+	let k=k+1
+done < /tmp/t.lis
 
 echo "Done"
