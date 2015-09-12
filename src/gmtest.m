c2984ed152a7ae3a76d0bf5a5491290338164016
Joaquim Luis <jmfluis@gmail.com>
2015-09-12 18:26:15 +0000

    Code clean up
    git-svn-id: svn://gmtserver.soest.hawaii.edu/gmt-mex/trunk@379 03974b7a-4592-4726-bb37-2cc38c695039

--- src/gmtest.m
+++ src/gmtest.m
@@ -2,16 +2,14 @@ function out = gmtest(test)
 	GRAPHICSMAGICK = 'C:/programs/GraphicsMagick/gm.exe';
 
 	[ps, t_path] = gallery(test);
+	if (isempty(ps))
+		disp(['Test -> ' test ' <- does not exist or some other error occurred']),	return
+	end
 	[pato, fname] = fileparts(ps);
 	png_name = [pato filesep fname '.png'];
 	ps_orig  = [t_path fname '.ps'];
-% 	cmd = ['gmt(psconvert -Tf -A -P ' psname ')'];
-% 	system(cmd)
 
-	% Compare the ps file with its original. Check $ps against original $ps or against $1.ps (if $1 given)
-	
-	% syntax: gm compare [ options ... ] reference-image [ options ... ] compare-image [ options ... ]
-	%rms=$("${GRAPHICSMAGICK}" compare -density 200 -maximum-error 0.001 -highlight-color magenta -highlight-style assign -metric rmse -file "${ps%.ps}.png" "$ps" "$src/${psref:-$ps}") || pscmpfailed="yes"
+	% Compare the ps file with its original.
 	cmd = [GRAPHICSMAGICK ' compare -density 200 -maximum-error 0.001 -highlight-color magenta -highlight-style' ...
 		' assign -metric rmse -file ' png_name ' ' ps_orig ' ' ps];
 	[status, cmdout] = system(cmd);
@@ -24,6 +22,7 @@ function out = gmtest(test)
 			disp(['Test ' test ' FAIL with: ' t(ind:end)])
 		else
 			disp(['Test ' test ' PASS'])
+			builtin('delete', png_name);
 		end
 % 	else
 % 		disp(['GRAPHICSMAGICK failed to run test: ' test])
