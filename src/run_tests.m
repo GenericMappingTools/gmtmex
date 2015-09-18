6e0ef3f9d297689c9ae356eff741e66f85b80aa3
Joaquim Luis <jmfluis@gmail.com>
2015-09-18 16:03:27 +0000

    know how to call the ported tests so far.
    git-svn-id: svn://gmtserver.soest.hawaii.edu/gmt-mex/trunk@384 03974b7a-4592-4726-bb37-2cc38c695039

--- src/run_tests.m
+++ src/run_tests.m
@@ -1,11 +1,26 @@
 function run_tests(what)
-%	$Id$
 % Run 'packages' of examples/tests
+% WHAT can be 'all_examples' to run all examples or 'all_tests' for the ported tests
+
 	if (strcmp(what, 'all_examples'))
 		for (k = 1:45)
 			ex = sprintf('ex%.2d', k);
 			gmtest(ex)
 		end
-	else
+	elseif (strcmp(what, 'all_tests'))
+		do_tests()
 	end
 
+% -------------------------------------------------------------------
+function do_tests()
+% Run all tests that we have ported so far
+	tests = {
+		'poldecimate','gmtspatial'
+		'measure','gmtspatial'
+		'readwrite_withgdal','grdimage'
+		'vectors','gmtvector'
+		};
+	
+	for (k = 1:size(tests,1))
+		gmtest(tests{k,1}, tests{k,2})
+	end
