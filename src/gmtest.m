e8d5513be2ab907d112960391028996a5cab4379
Joaquim Luis <jmfluis@gmail.com>
2015-09-18 13:41:18 +0000

    Add knowledge on how to call the tests from the test suite
    git-svn-id: svn://gmtserver.soest.hawaii.edu/gmt-mex/trunk@383 03974b7a-4592-4726-bb37-2cc38c695039

--- src/gmtest.m
+++ src/gmtest.m
@@ -1,7 +1,24 @@
-function out = gmtest(test)
+function out = gmtest(test, test_dir)
+% ...
+	global g_root_dir out_path;
+	% Edit those two for your own needs
+	g_root_dir = 'C:/progs_cygw/GMTdev/gmt5/branches/5.2.0/';
+	out_path = 'V:/';		% Set this if you want to save the PS files in a prticular place
 	GRAPHICSMAGICK = 'C:/programs/GraphicsMagick/gm.exe';
 
-	[ps, t_path] = gallery(test);
+	if ((nargin == 1) && (numel(test) == 4) && strncmp(test, 'ex', 2))	% Run example from gallery
+		[ps, t_path] = gallery(test);
+	else
+		[ps, t_path] = call_test(test, test_dir);
+		if (isa(ps, 'logical'))		% In this cases we have no PS files but only an error status from function
+			if (ps),		disp(['Test ' test ' PASS'])
+			else			disp(['Test ' test ' FAIL'])
+			end
+			if (nargout),	out = [];	end
+			return
+		end
+	end
+
 	if (isempty(ps))
 		disp(['Test -> ' test ' <- does not exist or some other error occurred']),	return
 	end
@@ -14,22 +31,35 @@ function out = gmtest(test)
 		' assign -metric rmse -file ' png_name ' ' ps_orig ' ' ps];
 	[status, cmdout] = system(cmd);
 	
-% 	if (status == 1)
-		n_lines = find(cmdout == sprintf('\n'));
+	n_lines = find(cmdout == sprintf('\n'));
+ 	if (status == 1)		% GM returned an error status
 		if (numel(n_lines) == 8)
 			t = cmdout(n_lines(end-1)+1:end-1);		% Last line containing the failing info
 			ind = strfind(t, 'image');
 			disp(['Test ' test ' FAIL with: ' t(ind:end)])
 		else
-			disp(['Test ' test ' PASS'])
-			builtin('delete', png_name);
+			disp(['GRAPHICSMAGICK failed to run test: ' test])
+			disp(cmdout)
 		end
-% 	else
-% 		disp(['GRAPHICSMAGICK failed to run test: ' test])
-% 	end
+	else
+		disp(['Test ' test ' PASS'])
+		builtin('delete', png_name);
+	end
 	
 	if (nargout)
 		out = cmdout;
 	end
 	
-	
+% ---------------------------------------------------------------------------------------
+function [ps, t_path] = call_test(test, test_dir)
+	global g_root_dir out_path
+	pato = [g_root_dir 'test/' test_dir];
+	addpath(pato)		% Need this because that's the only way I found to make str2func() work
+	try
+		[ps, t_path] = feval(str2func(test), out_path);
+	catch
+		disp(lasterr)
+		ps = [];	t_path = [];	% Defaults for the case of error
+	end
+	rmpath(pato)
+
