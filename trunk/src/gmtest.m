901bc7d0fb116a19e0266dcf797dea8923d2dc96
Joaquim Luis <jmfluis@gmail.com>
2015-09-24 15:51:10 +0000

    Add documentation Learned how to deal with tests from different sources
    git-svn-id: svn://gmtserver.soest.hawaii.edu/gmt-mex@388 03974b7a-4592-4726-bb37-2cc38c695039

--- trunk/src/gmtest.m
+++ trunk/src/gmtest.m
@@ -1,15 +1,29 @@
-function out = gmtest(test, test_dir)
-% ...
+function out = gmtest(test, test_dir, family)
+% Run a  test from the GALLERY, tests or doc/scripts
+%
+% gmtest(test) run an example from the gallery were TEST is is either 'ex01' ... 'ex45'
+%
+% gmtest(test, test_dir) runs a test from the tests suit. TEST_DIR is the directory name where TEST lieve
+%		example: gmtest('poldecimate','gmtspatial')
+%
+% gmtest(test, test_dir, family) runs a test from a certainly family of tests.
+%	Currently only family = 'scripts' is implemented (means run test from the 'scripts' dir. TEST_DIR is ignored
+%		example: gmtest('GMT_insert', '', 'scripts')
+
+%	$Id$
+
 	global g_root_dir out_path;
 	% Edit those two for your own needs
 	g_root_dir = 'C:/progs_cygw/GMTdev/gmt5/branches/5.2.0/';
 	out_path = 'V:/';		% Set this if you want to save the PS files in a prticular place
 	GRAPHICSMAGICK = 'C:/programs/GraphicsMagick/gm.exe';
 
+	if (nargin < 3),	family = '';	end
+
 	if ((nargin == 1) && (numel(test) == 4) && strncmp(test, 'ex', 2))	% Run example from gallery
-		[ps, t_path] = gallery(test);
+		[ps, orig_path] = gallery(test, g_root_dir, out_path);
 	else
-		[ps, t_path] = call_test(test, test_dir);
+		[ps, orig_path] = call_test(test, test_dir, g_root_dir, out_path, family);
 		if (isa(ps, 'logical'))		% In this cases we have no PS files but only an error status from function
 			if (ps),		disp(['Test ' test ' PASS'])
 			else			disp(['Test ' test ' FAIL'])
@@ -24,7 +38,7 @@ function out = gmtest(test, test_dir)
 	end
 	[pato, fname] = fileparts(ps);
 	png_name = [pato filesep fname '.png'];
-	ps_orig  = [t_path fname '.ps'];
+	ps_orig  = [orig_path fname '.ps'];
 
 	% Compare the ps file with its original.
 	cmd = [GRAPHICSMAGICK ' compare -density 200 -maximum-error 0.001 -highlight-color magenta -highlight-style' ...
@@ -51,15 +65,28 @@ function out = gmtest(test, test_dir)
 	end
 	
 % ---------------------------------------------------------------------------------------
-function [ps, t_path] = call_test(test, test_dir)
-	global g_root_dir out_path
-	pato = [g_root_dir 'test/' test_dir];
+function [ps, orig_path] = call_test(test, test_dir, g_root_dir, out_path, family)
+% Here PS hold the full name of the created PS file and ORIG_PATH must
+% contain the path to where the original postscript file, against which the PS will be compared
+
+	ps = '';	orig_path = '';		% Defaults for the case of error
+	if (isempty(family))						% A test from the tests suit
+		pato = [g_root_dir 'test/' test_dir];
+	elseif (strcmp(family, 'scripts'))			% A test for the 'scripts' directory
+		pato = [g_root_dir 'doc/scripts/ml/'];
+	else
+		disp(['Family ' family ' is unknown or not yet implemented.'])
+		return
+	end
+
 	addpath(pato)		% Need this because that's the only way I found to make str2func() work
 	try
-		[ps, t_path] = feval(str2func(test), out_path);
+		[ps, orig_path] = feval(str2func(test), out_path);
+		if (strcmp(family, 'scripts'))
+			orig_path = orig_path(1:end-3);		% Because original PS lieve in a subdir below
+		end
 	catch
 		disp(lasterr)
-		ps = [];	t_path = [];	% Defaults for the case of error
 	end
 	rmpath(pato)
 
