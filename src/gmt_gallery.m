b3e87206ec23a5085eb033e92328db069b7d7a0b
Joaquim Luis <jmfluis@gmail.com>
2017-10-23 01:19:59 +0000

    Fix ex28
    git-svn-id: svn://gmtserver.soest.hawaii.edu/gmt-mex/trunk@651 03974b7a-4592-4726-bb37-2cc38c695039

--- src/gmt_gallery.m
+++ src/gmt_gallery.m
@@ -1186,7 +1186,7 @@ function [ps, d_path] = ex28(g_root_dir, out_path, verbose)
 	% Overlay geographic data and coregister by using correct region and projection with the same scale
 	gmt(['pscoast -R@Kilauea.utm.nc -Ju5Q/1:160000 -O -K -Df+ -Slightblue -W0.5p -B5mg5m -BNE' ...
 		' --FONT_ANNOT_PRIMARY=12p --FORMAT_GEO_MAP=ddd:mmF >> ' ps])
-	gmt(['pstext -R -J -O -K -F+f12p,Helvetica-Bold+jCB >> ' ps], {'155:16:20W 19:26:20N KILAUEA'})
+	gmt(['pstext -R -J -O -K -F+f12p,Helvetica-Bold+jCB >> ' ps], record([-155.272222222222 19.4388888888889], 'KILAUEA'))
 	gmt(['psbasemap -R -J -O -K --FONT_ANNOT_PRIMARY=9p -LjRB+c19:23N+f+w5k+l1:16,000+u+o0.2i' ...
 		' --FONT_LABEL=10p >> ' ps])
 	% Annotate in km but append ,000m to annotations to get customized meter labels
